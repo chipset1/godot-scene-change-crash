8877ca260cece756c1c28e3cb0cc8654fc523d58
Stephen Emblem <4261118+chipset1@users.noreply.github.com>
2024-12-11 14:35:22 -0500

    Add different camera modes. Follow player or static and centered

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
 @onready var player : CharacterBody2D = $Player
+@onready var _camera : Camera2D = $Camera2D
 var _full_screen_size = Vector2(1920, 1080)
 var _windowed_size := Vector2(_full_screen_size.x*0.6, _full_screen_size.y*0.6)
 @onready var _current_level = $Levels/StartLevel
+
 
 ## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.change_level.connect(_main_change_level)
 	_set_player_start_position("StartLevel")
-	player.disable_camera_follow()
+	_should_update_camera()
 
 func _unhandled_input(_event: InputEvent) -> void:
 	# todo should I be doing this instead of using the _event
@@ -21,25 +23,41 @@ func _unhandled_input(_event: InputEvent) -> void:
 			DisplayServer.window_set_mode( DisplayServer.WINDOW_MODE_WINDOWED)
 			get_viewport().get_window().size = _windowed_size
 
-func _main_change_level(level_path : String):
-	var s = "res://scenes/levels/" + level_path + ".tscn"
-	_goto_scene(s)
+func _set_camera_center():
+	_camera.set_anchor_mode(Camera2D.ANCHOR_MODE_DRAG_CENTER)
 
-func _set_player_start_position(scene_name):
-	print(scene_name)
-	var player_start_position_node = _current_level.get_node(scene_name+"Enter");
+func _set_camera_top_left():
+	_camera.global_position = Vector2(0, 0)
+	_camera.set_anchor_mode(Camera2D.ANCHOR_MODE_FIXED_TOP_LEFT)
+
+func _main_change_level(level_path : String):
+	var l = "res://scenes/levels/" + level_path + ".tscn"
+	_goto_level(l)
+
+func _set_player_start_position(level_name):
+	var player_start_position_node = _current_level.get_node(level_name+"Enter");
 	if(player_start_position_node != null):
 		player.transform.origin = player_start_position_node.transform.origin
+	else:
+		push_warning("could not set player position on level change: " + level_name) 
 
+func _should_update_camera():
+	if _current_level.has_node("StaticCamera"):
+		_set_camera_top_left()
+		player.disable_camera_follow()
+	else:
+		_set_camera_center()
+		player.enable_camera_follow()
 
-func _goto_scene(path):
+func _goto_level(path):
 	# It is now safe to remove the current scene.
 	var previous_name = _current_level.name
 	_current_level.free()
 	# Load the new scene.
-	var s = ResourceLoader.load(path)
+	var l = ResourceLoader.load(path)
 	# Instance the new scene.
-	_current_level = s.instantiate()
+	_current_level = l.instantiate()
 	_set_player_start_position(previous_name)
+	_should_update_camera()
 	# Add it to the active scene, as child of root.
 	$Levels.add_child(_current_level)
